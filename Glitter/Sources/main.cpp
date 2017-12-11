18bce40927cb1da32fbc945001e8495a7f7e6187
Brad Kotsopoulos <bkotsopoulos@snapchat.com>
2017-12-10 23:57:46 -0500

    Movement, textures, etc working.

--- Glitter/Sources/main.cpp
+++ Glitter/Sources/main.cpp
@@ -22,22 +22,65 @@ void processInput(GLFWwindow* window) {
   }
 }
 
+// set up vertex data (and buffer(s)) and configure vertex attributes
+// ------------------------------------------------------------------
+float vertices[] = {
+   0.5f,  0.5f,  0.5f,  1.0f, 1.0f, // top right
+   0.5f, -0.5f,  0.5f,  1.0f, 0.0f, // bottom right
+  -0.5f, -0.5f,  0.5f,  0.0f, 0.0f, // bottom left
+  -0.5f,  0.5f,  0.5f,  0.0f, 1.0f, // top left
+   0.5f,  0.5f, -0.5f,  1.0f, 1.0f, // top right
+   0.5f, -0.5f, -0.5f,  1.0f, 0.0f, // bottom right
+  -0.5f, -0.5f, -0.5f,  0.0f, 0.0f, // bottom left
+  -0.5f,  0.5f, -0.5f,  0.0f, 1.0f, // top left
+};
+const int vertexAttribStride = 5 * sizeof(float);
+
+unsigned int indices[] = {  // note that we start from 0!
+  0, 1, 3,  // first Triangle
+  1, 2, 3,   // second Triangle
+  0, 1, 4,
+  1, 4, 5,
+  1, 2, 5,
+  2, 5, 6,
+  2, 3, 6,
+  3, 6, 7,
+  0, 3, 4,
+  3, 4, 7,
+  4, 5, 6,
+  4, 6, 7,
+};
+const int numIndices = sizeof(indices) / sizeof(unsigned int);
+
 const std::string vertexShaderSourceStr = R"(
 #version 330 core
 layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec2 aTexCoord;
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
+out vec2 TexCoord;
 
 void main()
 {
-  gl_Position = vec4(aPos, 1.0);
+  TexCoord = aTexCoord;
+  gl_Position = projection * view * model * vec4(aPos, 1.0);
 }
 )";
 
 const std::string fragmentShaderSourceStr = R"(
 #version 330 core
+
+in vec2 TexCoord;
 out vec4 FragColor;
+
+uniform sampler2D texture1;
+
 void main()
 {
-  FragColor = vec4(0.0f, 0.0f, 1.0f, 1.0f);
+  FragColor = mix(texture(texture1, TexCoord), vec4(1.0, 0.0, 0.0, 1.0), 0.1);
 }
 )";
 
@@ -90,24 +133,11 @@ int setupShaders() {
 }
 
 void setupVertexData(unsigned int* VBO, unsigned int* VAO, unsigned int* EBO) {
-  // set up vertex data (and buffer(s)) and configure vertex attributes
-  // ------------------------------------------------------------------
-  float vertices[] = {
-    0.5f,  0.5f, 0.0f,  // top right
-    0.5f, -0.5f, 0.0f,  // bottom right
-    -0.5f, -0.5f, 0.0f,  // bottom left
-    -0.5f,  0.5f, 0.0f   // top left
-  };
-
-  unsigned int indices[] = {  // note that we start from 0!
-    0, 1, 3,  // first Triangle
-    1, 2, 3   // second Triangle
-  };
-
   glGenVertexArrays(1, VAO);
   glGenBuffers(1, VBO);
   glGenBuffers(1, EBO);
-  // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+  // bind the Vertex Array Object first, then bind and set vertex buffer(s),
+  // and then configure vertex attributes(s).
   glBindVertexArray(*VAO);
 
   glBindBuffer(GL_ARRAY_BUFFER, *VBO);
@@ -116,18 +146,68 @@ void setupVertexData(unsigned int* VBO, unsigned int* VAO, unsigned int* EBO) {
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, *EBO);
   glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-  glEnableVertexAttribArray(0);
+  {
+    const int vertexAttribIndex = 0;
+    const int componentsPerAttrib = 3;
+    void* startOffsetPointer = (void*)(sizeof(float) * 0);
+    glVertexAttribPointer(vertexAttribIndex, componentsPerAttrib, GL_FLOAT, GL_FALSE, vertexAttribStride, startOffsetPointer);
+    glEnableVertexAttribArray(vertexAttribIndex);
+  }
+  {
+    const int vertexAttribIndex = 1;
+    const int componentsPerAttrib = 2;
+    void* startOffsetPointer = (void*)(sizeof(float) * 3);
+    glVertexAttribPointer(vertexAttribIndex, componentsPerAttrib, GL_FLOAT, GL_FALSE, vertexAttribStride, startOffsetPointer);
+    glEnableVertexAttribArray(vertexAttribIndex);
+  }
 
-  // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+  // note that this is allowed, the call to glVertexAttribPointer registered
+  // VBO as the vertex attribute's bound vertex buffer object so afterwards we
+  // can safely unbind
   glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-  // remember: do NOT unbind the EBO while a VAO is active as the bound element buffer object IS stored in the VAO; keep the EBO bound.
+  // remember: do NOT unbind the EBO while a VAO is active as the bound element
+  // buffer object IS stored in the VAO; keep the EBO bound.
   //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
-  // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
-  // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+  // You can unbind the VAO afterwards so other VAO calls won't accidentally
+  // modify this VAO, but this rarely happens. Modifying other VAOs requires
+  // a call to glBindVertexArray anyways so we generally don't unbind VAOs
+  // (nor VBOs) when it's not directly necessary.
   glBindVertexArray(0);
+}
+
+unsigned int setupTextures() {
+  // load and create a texture
+  // -------------------------
+  unsigned int texture1;
+
+  glGenTextures(1, &texture1);
+  glBindTexture(GL_TEXTURE_2D, texture1);
+
+  // set the texture wrapping parameters
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+  // set texture filtering parameters
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+  // load image, create texture and generate mipmaps
+  int width, height, nrChannels;
+  stbi_set_flip_vertically_on_load(true); // tell stb_image.h to flip loaded texture's on the y-axis.
+  unsigned char *data = stbi_load("/Users/bradkotsopoulos/Snapchat/Dev/Glitter/bitmoji.png", &width, &height, &nrChannels, 0);
+  if (data) {
+    std::cout << "WxH: " << width << "x" << height << std::endl;
+    glTexImage2D(GL_TEXTURE_2D, 0 /* levelOfDetail */, GL_RGB, width, height, 0 /* always zero */, GL_RGBA, GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+  } else {
+    std::cout << "Failed to load texture" << std::endl;
+  }
+
+  stbi_image_free(data);
+
+  return texture1;
 }
 
 int main() {
@@ -153,9 +233,13 @@ int main() {
   fprintf(stderr, "OpenGL %s\n", glGetString(GL_VERSION));
 
   int shaderProgram = setupShaders();
+  glUseProgram(shaderProgram);
+  glUniform1i(glGetUniformLocation(shaderProgram, "texture1"), 0);
 
   unsigned int VBO, VAO, EBO;
   setupVertexData(&VBO, &VAO, &EBO);
+
+  unsigned int texture1 = setupTextures();
 
   // uncomment this call to draw in wireframe polygons.
 //  glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
@@ -165,20 +249,37 @@ int main() {
     processInput(mWindow);
 
     // Background Fill Color
-    glClearColor(1.0f, 0.25f, 0.25f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
+    glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-//    glm::mat4 transform;
-//    transform = glm::translate(transform, glm::vec3(0.5f, -0.5f, 0.0f));
-//    transform = glm::rotate(transform, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
-//
-//    unsigned int transformLoc = glGetUniformLocation(shaderProgram, "transform");
-//    glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
+    glEnable(GL_DEPTH_TEST);
+
+    // bind textures on corresponding texture units
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture1);
 
     glUseProgram(shaderProgram);
+
+    glm::mat4 model = glm::mat4(1.0);
+    glm::mat4 view = glm::mat4(1.0);
+    glm::mat4 projection = glm::mat4(1.0);
+
+    model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(1.0f, 0.0f, 0.0f));
+    view  = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+    projection = glm::perspective(glm::radians(45.0f), (float)mWidth / (float)mHeight, 0.1f, 100.0f);
+
+    // retrieve the matrix uniform locations
+    unsigned int modelLoc      = glGetUniformLocation(shaderProgram, "model");
+    unsigned int viewLoc       = glGetUniformLocation(shaderProgram, "view");
+    unsigned int projectionLoc = glGetUniformLocation(shaderProgram, "projection");
+
+    // pass them to the shaders
+    glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+    glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+    glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
     glBindVertexArray(VAO);
-    //glDrawArrays(GL_TRIANGLES, 0, 6);
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    glDrawElements(GL_TRIANGLES, numIndices, GL_UNSIGNED_INT, 0);
     glBindVertexArray(0);
 
     // Flip Buffers and Draw
